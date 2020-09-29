abc15e2894adaa4984df751ec3062359a1db60e0
funnyboy-roks <funnyboyroks@gmail.com>
2020-09-28 20:28:11 -0500

    :sparkle:form validation

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function convertToMC() {
-  hexIn = document.querySelector("#hex-in").value;
-  hexIn = hexIn.replace("#", "");
-  let output = "Not A Hex Code";
+inField = document.querySelector("#hex-in");
+inField.value = location.hash;
+convertToMC(inField);
+
+function convertToMC(obj) {
+  obj.value = obj.value.replace(/[^0-9a-f#]/g, '');
+  let maxLen = obj.value.startsWith('#') ? 7 : 6;
+  obj.value = obj.value.substring(0, maxLen)
+
+  if (obj.value.startsWith('#')) {
+    obj.value = '#' + obj.value.substring(1).replace(/#/g, '');
+  } else {
+    obj.value = '#' + obj.value.replace(/#/g, '');
+
+  }
+
+  hexIn = obj.value.replace(/#/g, "");
+  output = "";
   matchRegex = hexIn.match(/[0-9a-f]{6}|[0-9a-f]{3}/i);
   if ((hexIn.length == 6 || hexIn.length == 3) && matchRegex) {
     if (matchRegex[0] == hexIn) {
@@ -17,9 +31,24 @@ function convertToMC() {
         output += `&${x}`;
       }
     }
+    var rgb = parseInt(hexIn, 16); // convert rrggbb to decimal
+    var r = (rgb >> 16) & 0xff; // extract red
+    var g = (rgb >> 8) & 0xff; // extract green
+    var b = (rgb >> 0) & 0xff; // extract blue
+
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+    if (luma < 100) {
+      document.querySelector("#out").style.color = "#fff";
+    } else {
+      document.querySelector("#out").style.color = "#000";
+    }
+  } else {
+    document.querySelector("#out").style.color = "#000";
   }
 
   // return hexIn
+  location.hash = hexIn;
   document.querySelector("html").style.background = "#" + hexIn;
   document.querySelector("#out").innerText = output;
 }
