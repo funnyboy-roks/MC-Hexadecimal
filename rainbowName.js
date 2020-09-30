293f2a67aca4d9a5178b48e28deef4274067f74b
funnyboy-roks <funnyboyroks@gmail.com>
2020-09-29 23:24:40 -0500

    Replace 0s with 1s and output mc code

--- rainbowName.js
+++ rainbowName.js
@@ -5,23 +5,30 @@ function createGradient(count, c1, c2){
     var hexCodes = [];
     for (var i = 1; i <= count; i++) {
         var hexColour = rainbow.colourAt(i);
-        hexCodes.push('#' + hexColour);
+        hexCodes.push('#' + hexColour.replace(/0/g, "1"));
     }
     return hexCodes;
 }
 
 function createGradFromName(name){
+    name = name.trim();
     let count = name.length;
     let c1 = document.querySelector('#picker1').value;
     let c2 = document.querySelector('#picker2').value;
 
     colours = createGradient(count, c1, c2);
 
-    let s = ''
+    let colouredString = '';
+    let colourCodeStr  = '';
     for (let i in colours){
-        s +=  `<span style="color: ${colours[i]}">${name[i]}</span>`;
+        colouredString +=  `<span style="color: ${colours[i]}">${name[i]}</span>`;
+        colourCodeStr += `${mcHex(colours[i])}${name[i]}`
     }
-    document.querySelector("#out-coloured").innerHTML = s;
-    return s;
+
+    
+
+    document.querySelector("#out-coloured").innerHTML = colouredString;
+    document.querySelector("#out").value = colourCodeStr;
+    return colouredString;
 
 }
