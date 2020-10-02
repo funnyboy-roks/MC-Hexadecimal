0d2e1cbc8b9f30568143fb0c6aacb814cf2235af
Luminic <48740008+Luminic@users.noreply.github.com>
2020-10-01 22:53:23 -0700

    Update rainbowName.js
    Add `createLinearGradient` function which will calculate the gradient in linear space. I also modified, the `createGradient` function to just call `createLinearGradient`.
    Two helper functions `hexStoF` & `ftoHexS` were also created to convert from hex to float and back.

--- rainbowName.js
+++ rainbowName.js
@@ -1,7 +1,76 @@
 const coloursOutLabel = document.querySelector("#colours");
 const nickOutLabel = document.querySelector("#nick")
 
+function hexStoF(hexidecimalString) {
+    // Takes a 2 digit hex value as a string (ex: "2f")
+    // Returns the value as a float from 0-1 (ex: 0.1843)
+    // Warning: Rounding errors may result in a returned float slightly ouside of the 0-1 range
+
+    // parseInt understands a number starting w/ 0x to be hex
+    let asInt = parseInt("0x" + hexidecimalString);
+    return asInt/255.0;
+}
+
+function ftoHexS(floatCol) {
+    // Takes a float value from 0-1 (ex: 0.1843)
+    // Returns the corresponding hex value from 0x00 to 0xff as a string (ex: "2f")
+    // The return value will: always have 2 digits, exclude the "0x", be clamped to 0x00-0xff
+
+    let asInt = Math.round(floatCol*255);
+    asInt = Math.min(Math.max(0, asInt), 255); // Clamp from 0-255
+    let asHexStr = asInt.toString(16);
+    asHexStr = "0".repeat(2-asHexStr.length) + asHexStr;
+    return asHexStr;
+}
+
+const gamma = 2.2;
+
+function createLinearGradient(count, coloursArr) {
+    var rainbow = new Rainbow(); 
+
+    // Convert the color into linear space
+    for (let i=0; i<coloursArr.length; i++) {
+        colour = [
+            coloursArr[i].substring(1,3),
+            coloursArr[i].substring(3,5),
+            coloursArr[i].substring(5,7)
+        ];
+        coloursArr[i] = "#"
+        for (let j=0; j<colour.length; j++) {
+            colour[j] = hexStoF(colour[j]); // Convert to float
+            colour[j] = colour[j]**gamma; // Actual gamma->linear conversion
+            coloursArr[i] += ftoHexS(colour[j]); // Convert back to hex
+        }
+    }
+
+    rainbow.setNumberRange(1, count);
+    rainbow.setSpectrumByArray(coloursArr);
+    var hexCodes = [];
+    for (var i = 1; i <= count; i++) {
+        var hexColour = rainbow.colourAt(i);
+
+        // Convert the color back into gamma space
+        colour = [
+            hexColour.substring(0,2),
+            hexColour.substring(2,4),
+            hexColour.substring(4,6)
+        ];
+        hexColour = "";
+        for (let j=0; j<colour.length; j++) { 
+            colour[j] = hexStoF(colour[j]); // Convert to float
+            colour[j] = colour[j]**(1/gamma); // Actual linear->gamma conversion
+            hexColour += ftoHexS(colour[j]); // Convert back to hex
+        }
+
+        hexCodes.push('#' + hexColour.replace(/0/g, "1"));
+    }
+    return hexCodes;
+}
+
 function createGradient(count, coloursArr){
+    // Temporarily use linear gradient by default until option to switch mode is made
+    return createLinearGradient(count, coloursArr);
+
     var rainbow = new Rainbow(); 
     rainbow.setNumberRange(1, count);
     rainbow.setSpectrumByArray(coloursArr);
